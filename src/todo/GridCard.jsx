76edbf81d6599b96d14ea841c8303ec50c517564
kevindantas <kevin.ds.29@gmail.com>
2016-09-27 10:10:27 -0300

    Filter cards and show feedback for loading and not found

--- src/todo/GridCard.jsx
+++ src/todo/GridCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Card from './Card';
 
@@ -15,11 +15,10 @@ class GridCard extends Component {
 
     // Declara um valor padrão para o state todos que estamos usando
     this.state = {
-      todos: [],
+      todos:  null,
       search: ''
     }
   }
-
 
 
   handleSearch(value) {
@@ -29,8 +28,6 @@ class GridCard extends Component {
   }
 
 
-
-
   /**
    * Renderiza os nossos Cards
    *
@@ -38,20 +35,47 @@ class GridCard extends Component {
    */
   renderCards() {
 
-    // A função .indexOf pesquisa a ocorrência de um caracter em uma string
-    // Retorna um inteiro
-    //   -1 para caso não ache nenhuma ocorrência
-    //   Ou o indice daquela ocorrencia
-    // Ex.:
-    //   var a = 'abcdefgh';
-    //   a.indexOf('c'); // retorna 2 (0 = a, 1, = b, 2 = c)
+    const styleH3 = {
+      textAlign: 'center'
+    };
 
-    return this.state.todos.filter(todo => {
+
+    var todoList = this.state.todos;
+
+
+    // Se o state.todos está null, então mostramos a mensagem de carregando
+    if (!todoList) {
+      return (<h3 style={styleH3}>Carregando...</h3>); // return para a a função
+    }
+
+
+    /***********************  Filtra os todos  **********************
+    *                                                               *
+    *   A função indexOf pesquisa um caracter em uma string/array   *
+    *   E Retorna um inteiro:                                       *
+    *     -1 para caso não ache nenhuma ocorrência                  *
+    *     Ou o indice daquela ocorrencia caso ache alguma coisa     *
+    *   Ex.:                                                        *
+    *     var a = 'abcdefgh';                                       *
+    *     a.indexOf('c'); // retorna 2 (0 = a, 1, = b, 2 = c)       *
+    *                                                               *
+    *****************************************************************/
+    var todoListFiltered = todoList.filter(todo => {
       return todo.titulo.toLocaleLowerCase().indexOf(this.state.search) > -1;
-    }).map(todo => {
-      return <Card key={todo.id} todo={todo} />
+    });
+
+
+    // Se a quantidade de todos filtrados for menor que 1, mostra mensagem de não encontrado
+    if (todoListFiltered < 1) {
+      return (<h3 style={styleH3}>Nenhum resultado encontrado!</h3>);
+    }
+
+
+    return todoListFiltered.map(todo => {
+      return <Card key={todo.id} todo={todo}/>
     });
   }
+
 
   /**
    * Faz a requisição para a API
