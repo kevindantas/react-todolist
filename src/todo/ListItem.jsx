fcb8816800592c1cbc80600627a9a4f900c8ea87
kevindantas <kevin.ds.29@gmail.com>
2016-09-27 16:20:48 -0300

    Check list item

--- src/todo/ListItem.jsx
+++ src/todo/ListItem.jsx
@@ -2,11 +2,39 @@ import React, { Component, PropTypes } from 'react';
 
 
 class ListItem extends Component {
+
+  componentWillMount() {
+    this.state = {
+      done: this.props.item.feito
+    };
+  }
+
+
+  checkItem(e) {
+
+    var isChecked = e.target.checked;
+
+    this.setState({
+      done: isChecked
+    });
+    
+    fetch(`https://private-00cf6-reacttodo.apiary-mock.com/item/${this.props.item.id}`, {
+      method: 'put',
+      body: JSON.stringify({feito: isChecked})
+    });
+
+  }
+
+
   render() {
+    var classNames = `list-item `;
+    if(this.state.done)
+      classNames += 'is-done';
+
     return (
-      <li>
+      <li className={classNames}>
         <label>
-          <input type="checkbox"/>
+          <input type="checkbox" defaultChecked={this.props.item.feito} onChange={this.checkItem.bind(this)} />
           { this.props.item.descricao }
         </label>
 
